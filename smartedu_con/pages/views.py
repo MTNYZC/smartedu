3b349fcabccbbee576e539ae1299b4b525534b7a
MetinYazıcı <yazicimetin19@gmail.com>
2023-07-31 16:26:01 +0300

    Templates dosyası eklendi

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def index(request):
-    return HttpResponse("<h1>İNDEX SAYFASI 10</h1>")
+    return render (request, 'index.html')
 
 
