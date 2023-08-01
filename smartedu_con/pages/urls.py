8cb0f9d692f568bcd5f30d413f8055c76499baae
MetinYazıcı <yazicimetin19@gmail.com>
2023-08-01 15:55:20 +0300

    Partials,URL,Menu,Extends-Include düzenlendi

--- smartedu_con/pages/urls.py
+++ smartedu_con/pages/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
     # path(route, views , opt(kısayol ismi ))
+    
 ]
--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -6,3 +6,6 @@ def index(request):
     return render (request, 'index.html')
 
 
+
+def about(request):
+    return render (request, 'about.html')
